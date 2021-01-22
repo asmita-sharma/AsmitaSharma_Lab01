932582b1a70be7c8f967ec5e3e31e83825e24912
asmita-sharma <70826237+asmita-sharma@users.noreply.github.com>
2021-01-22 17:13:51 -0500

    Add project files.

---
+++ AsmitaSharma_Lab01/CreateBucketResponse.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AsmitaSharma_Lab01
+{
+    class CreateBucketResponse
+    {
+        private string bucketName;
+        public string RequestId;
+
+        private string name; // field
+
+        
+
+        public string BucketName   // property
+        {
+            get { return bucketName; }   // get method
+            set { bucketName = value; }  // set method
+        }
+
+        public string RequestID  // property
+        {
+            get { return RequestId; }   // get method
+            set { RequestId = value; }  // set method
+        }
+    }
+}
---
+++ AsmitaSharma_Lab01/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsmitaSharma_Lab01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string name;
+        string awsAccessKey = "AKIAYJ5X7MERCVV5QOGS";
+        string awsSecreteKey = "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";
+        private static readonly RegionEndpoint bucketRegion = RegionEndpoint.USEast1;
+        private static IAmazonS3 s3Client;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        
+
+        private void createButton_Click(object sender, RoutedEventArgs e)
+        {
+            s3Client = new AmazonS3Client(awsAccessKey, awsSecreteKey, RegionEndpoint.USEast1);
+            CreateBucket(textBox1.Text).Wait();
+            
+        }
+
+        //private static BasicAWSCredentials GetBasicCredentials()
+        //{
+        //    var builder = new ConfigurationBuilder
+        //        .SetBasePath(Directory.GetCurrentDirectory())
+        //        .AddJsonFile("AppSettings.json", optional: true, reloadOnChange: true);
+
+        //    var accessID = builder.Build().GetSection("awsAccessKey").Value;
+        //    var secretKey = builder.Build().GetSection("awsSecretKey").Value;
+        //}
+
+        private static async Task<CreateBucketResponse> CreateBucket(string bucketName)
+        {
+            var putBucketRequest = new PutBucketRequest
+            {
+                BucketName = bucketName,
+                UseClientRegion = true
+            };
+
+            var response = await s3Client.PutBucketAsync(putBucketRequest);
+
+            return new CreateBucketResponse
+            {
+                BucketName = bucketName,
+                RequestId = response.ResponseMetadata.RequestId
+            };
+
+            MessageBox.Show("Bucket Created");
+        }
+
+    }
+}
